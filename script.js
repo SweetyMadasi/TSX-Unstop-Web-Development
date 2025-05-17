648ab87e4b3e042ccf562d94e5fb6ffc45b7d0b9
Sweety Madasi <madasisweety771@gmail.com>
2025-05-17 18:53:55 +0530

    Initial commit with HTML,CSS, JS

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("page loaded successfully!");
+
+//Add behavior to the button
+document.querySelector("button").addEventListener("click", function () {
+alert("you clicked the button!");
+});
